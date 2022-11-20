f23e1e0447780bef1d92061ac960f57fe1649f1d
Lety <taej0127@gmail.com>
2022-11-20 18:19:09 +0900

    Head Image Script component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+// const YourComponent = () => (
+//   <Image
+//     src="/images/profile.jpg" // Route of the image file
+//     height={144} // Desired size with correct aspect ratio
+//     width={144} // Desired size with correct aspect ratio
+//     alt="Your Name"
+//   />
+// );
+
 export default function Home() {
   return (
     <div className="container">
